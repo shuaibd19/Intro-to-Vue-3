3da3291d47540e8cd6e6288313d1c00d732640c1
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2020-12-15 16:04:51 +1100

    up to class 6

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "Second hand used socks for the kinky people out there",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://codepen.io/VueMastery/project/editor/AqemLM",
+      inStock: true,
+      inventory: 5,
+      onSale: true,
+      details: ["50% cotton", "30% wool", "20% pure unadulterated badass"],
+      variants: [
+        { id: 1234, colour: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 1235, colour: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+      cart: 0,
+    };
+  },
+  methods: {
+    onAddToCart() {
+      this.cart += 1;
+    },
+    onEmptyCart() {
+      this.cart = 0;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+  },
+});
